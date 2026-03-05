5dca030189ea37a293443d86c02cc0da63fbd647
Alesteen-Sharujan <alesteensharu@gmail.com>
2026-03-05 20:52:28 +0530

    creating a server

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ fs.writeFile('file2.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
 });
+
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
